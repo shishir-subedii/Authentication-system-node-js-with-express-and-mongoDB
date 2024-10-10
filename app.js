801cb3a69ba5030496d8bbfd70ff55c7029b4e37
Shishir <shishir16subedi@gmail.com>
2024-10-10 17:15:21 +0545

    added cors

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const dbConnect = require('./config/db'); // Ensure the correct path to db.js
-
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors)
 app.use('/api/auth', authRoutes);
 
 // Error handling middleware should be the last middleware
